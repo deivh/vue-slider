2348e519705cf3f3d872529cd9a10a7834455797
Davide Mannarelli <davidemannarelli24@gmail.com>
2021-04-07 14:55:21 +0200

    slider: right arrow button

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function initVue() {
+    new Vue({
+        el: '#app', 
+        data: {
+            'images' : 
+                        ["img/1.jfif",
+                         "img/2.jfif",
+                         "img/3.png",
+                         "img/4.jfif",
+                         "img/5.png",
+                         "img/6.jfif",
+                        ],
+            'img' : '',
+            'cont' : 0,
+
+        },
+        methods: {
+             slider: function(){
+                 this.img = this.images[this.cont];
+                 return this.img;
+             },
+             getClick: function() {
+                this.cont  += 1;
+                console.log(this.cont);
+                this.cont = this.cont >= this.images.length  ? 0 : this.cont = this.cont
+                 return this.cont;
+             }
+    }});
+}
+
+function init() {
+    initVue();
+
+}
+$(init);
